1e8fb53c66cd2220b3af2a5ad916fe1ea4756176
Hanibuchi <hanibuchikouto003@gmail.com>
2025-11-13 20:09:35 +0900

    杖クラス作成

---
+++ Assets/Scripts/Wand.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+/// <summary>
+/// 魔法の杖を表すクラス。
+/// 杖の種類と、セットされた呪文の配列を持ちます。
+/// </summary>
+[SerializeField]
+public class Wand
+{
+    [Header("杖の特性")]
+    [Tooltip("この杖の種類")]
+    public WandType type = WandType.Default;
+
+    [Header("呪文スロット")]
+    [Tooltip("この杖にセットされている呪文の配列。")]
+    [SerializeReference] public List<ISpell> spells = new List<ISpell>();
+
+    /// <summary>
+    /// 現在の杖にセットされている呪文リストを取得します。
+    /// </summary>
+    /// <returns>呪文のリスト</returns>
+    public List<ISpell> GetSpells()
+    {
+        return spells;
+    }
+
+    public void Add(ISpell spell)
+    {
+        spells.Add(spell);
+    }
+    public void Remove(ISpell spell)
+    {
+        spells.Remove(spell);
+    }
+}
+
+
+// 呪文を表すマーカーインターフェース 
+public interface ISpell
+{
+    // ISpellを継承したクラスが呪文として扱われます。
+}
+
+/// <summary>
+/// 杖の種類を定義する列挙型（Enum）。
+/// </summary>
+public enum WandType
+{
+    Default,
+    Crystal,
+    Wood,
+    Steam,
+    Dark,
+}
