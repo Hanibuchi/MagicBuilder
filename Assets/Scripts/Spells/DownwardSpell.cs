5d3fc2cf42e8a4e3e6ffed8538710eb378abc59a
Hanibuchi <hanibuchikouto003@gmail.com>
2025-11-16 20:45:31 +0900

    上昇呪文作成

--- Assets/Scripts/Spells/DownwardSpell.cs
+++ Assets/Scripts/Spells/DownwardSpell.cs
@@ -4,18 +4,19 @@ using System.Collections.Generic;
 [CreateAssetMenu(fileName = "DownwardSpell", menuName = "Wand System/Downward Spell")]
 public class Downward : SpellBase
 {
+    [SerializeField] bool upward = false;
     public override void DisplayAimingLine(
         List<SpellBase> wandSpells, int currentSpellIndex, float rotationZ,
         float strength, Vector2 casterPosition, bool clearLine = false)
     {
-        DisplayAimingLineForNextSpells(GetNextSpellOffsets(), wandSpells, currentSpellIndex, -90, strength, casterPosition, clearLine);
+        DisplayAimingLineForNextSpells(GetNextSpellOffsets(), wandSpells, currentSpellIndex, upward ? 90 : -90, strength, casterPosition, clearLine);
     }
 
     public override void FireSpell(
         List<SpellBase> wandSpells, int currentSpellIndex,
         float rotationZ, float strength, SpellContext context)
     {
-        FireSpellForNextSpells(GetNextSpellOffsets(), wandSpells, currentSpellIndex, -90, strength, context);
+        FireSpellForNextSpells(GetNextSpellOffsets(), wandSpells, currentSpellIndex, upward ? 90 : -90, strength, context);
     }
 
     int[] nextSpellOffsets = { 1 };
