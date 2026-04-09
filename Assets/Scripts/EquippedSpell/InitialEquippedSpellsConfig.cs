392cbea549f37e9cd0e056cced9cd121836ead2a
Hanibuchi <hanibuchikouto003@gmail.com>
2026-04-10 06:17:57 +0900

    初期スロット数変更

--- Assets/Scripts/EquippedSpell/InitialEquippedSpellsConfig.cs
+++ Assets/Scripts/EquippedSpell/InitialEquippedSpellsConfig.cs
@@ -8,6 +8,9 @@ using System.Collections.Generic;
 [CreateAssetMenu(fileName = "InitialEquippedSpellsConfig", menuName = "MagicBuilder/InitialEquippedSpellsConfig")]
 public class InitialEquippedSpellsConfig : ScriptableObject
 {
+    [Tooltip("初期状態での持ち込みスロット数（初期呪文数より少ない場合は初期呪文の数が優先されます）")]
+    public int initialCapacity = 3;
+
     [Tooltip("初期状態でスロットにセットされている呪文のリスト")]
     public List<SpellBase> initialSpells = new List<SpellBase>();
 }
