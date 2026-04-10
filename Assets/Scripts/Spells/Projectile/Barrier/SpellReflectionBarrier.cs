bc6d33cd5a48440de42c3b52eb0fff3c75d8a307
Hanibuchi <hanibuchikouto003@gmail.com>
2026-04-10 16:59:22 +0900

    反射バリアが多段ヒットを反転させるようにした。SEも鳴るようにした

--- Assets/Scripts/Spells/Projectile/Barrier/SpellReflectionBarrier.cs
+++ Assets/Scripts/Spells/Projectile/Barrier/SpellReflectionBarrier.cs
@@ -5,32 +5,58 @@ using UnityEngine;
 /// </summary>
 public class SpellReflectionBarrier : MonoBehaviour
 {
+    [Header("Audio")]
+    [SerializeField] private AudioClip reflectSE;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        HandleReflection(other.gameObject);
+        HandleReflection(other.gameObject, other.isTrigger);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        HandleReflection(collision.gameObject);
+        HandleReflection(collision.gameObject, collision.collider.isTrigger);
     }
 
     /// <summary>
     /// 対象のオブジェクトからDamageSourceBaseを探し、攻撃レイヤーを反転させます。
     /// </summary>
     /// <param name="go">衝突したオブジェクト</param>
-    private void HandleReflection(GameObject go)
+    private void HandleReflection(GameObject go, bool isTrigger)
     {
         // 親方向、またはルートから DamageSourceBase を探す
         DamageSourceBase ds = go.GetComponentInParent<DamageSourceBase>();
         
         if (ds != null)
         {
-            Reflect(ds);
+            bool reflected = Reflect(ds);
+
+            if (reflected)
+            {
+                // SEの再生
+                if (reflectSE != null && SoundManager.Instance != null)
+                {
+                    SoundManager.Instance.PlaySE(reflectSE);
+                }
+
+                // isTriggerがtrueの場合に方向と速度を反転
+                if (isTrigger)
+                {
+                    // 向いている方向を反転
+                    go.transform.root.Rotate(0, 0, 180);
+
+                    // 速度を反転
+                    Rigidbody2D rb = go.GetComponentInParent<Rigidbody2D>();
+                    if (rb != null)
+                    {
+                        rb.linearVelocity = -rb.linearVelocity;
+                    }
+                }
+            }
         }
     }
 
-    private void Reflect(DamageSourceBase ds)
+    private bool Reflect(DamageSourceBase ds)
     {
         SpellLayer currentLayer = ds.GetSpellLayer();
         
@@ -38,11 +64,15 @@ public class SpellReflectionBarrier : MonoBehaviour
         {
             ds.SetLayer(SpellLayer.Attack_Enemy);
             // Debug.Log($"[SpellReflectionBarrier] {ds.gameObject.name} を敵の攻撃に変更しました。");
+            return true;
         }
         else if (currentLayer == SpellLayer.Attack_Enemy)
         {
             ds.SetLayer(SpellLayer.Attack_Ally);
             // Debug.Log($"[SpellReflectionBarrier] {ds.gameObject.name} を味方の攻撃に変更しました。");
+            return true;
         }
+
+        return false;
     }
 }
